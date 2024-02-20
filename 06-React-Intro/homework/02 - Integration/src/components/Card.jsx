efb9050276f4297284453c9d61a356ca047b50a0
GuilleCOzzi <gecozzi87@gmail.com>
2024-02-20 10:11:17 -0300

    resuelto la homework 06 React Intro

--- 06-React-Intro/homework/02 - Integration/src/components/Card.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Card.jsx
@@ -1,13 +1,15 @@
-export default function Card(props) {
+import React from 'react';
+const Card = function (props) {
    return (
       <div>
-         {/* <button onClick={}>X</button>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <h2></h2>
-         <img src={} alt='' /> */}
+         <button onClick={props.onClose}>X</button>
+         <h2>{props.name}</h2>
+         <h2>{props.status}</h2>
+         <h2>{props.species}</h2>
+         <h2>{props.gender}</h2>
+         <h2>{props.origin}</h2>
+         <img src={props.image} alt='Rick Sanchez' />
       </div>
    );
 }
+export default Card;
--- 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
+++ 06-React-Intro/homework/02 - Integration/src/components/Cards.jsx
@@ -1,5 +1,20 @@
 import Card from './Card';
-
+import characters from '../data.js';
 export default function Cards(props) {
-   return <div></div>;
+   return <div>
+      {
+         characters.map((character) => {
+            return <Card
+               id={character.id}
+               name={character.name}
+               status={character.status}
+               species={character.species}
+               gender={character.gender}
+               origin={character.origin.name}
+               image={character.image}
+               onClose={() => window.alert('Emulamos que se cierra la card')}
+            />
+         })
+      }
+   </div>;
 }
