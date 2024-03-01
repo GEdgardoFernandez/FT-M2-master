f7ef9adbd8dd787bb284a7eecd4adfa7988a6557
GuilleCOzzi <gecozzi87@gmail.com>
2024-02-29 22:22:51 -0300

    resuelto el punto 1 de la Homework 08(falta revisar algunos puntos)

--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Animals/Animals.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
-// import styledAnimals from './Animals.module.css'
+import styledAnimals from './Animals.module.css'
+export default class Animals extends React.Component {  render() {
+  const { animals } = this.props;
 
-export default class Animals extends React.Component {
-  constructor(props) {
-    super();
-  }
-
-  render() {
-    return <div></div>
-  }
+  return (
+    <div className={styledAnimals.container}>
+      {animals.map((animal) => (
+        <div key={animal.id} className={styledAnimals.containerAnimals}>
+          <h5>{animal.name}</h5>
+          <img src={animal.image} alt={animal.name} width="300px" />
+          <span>{animal.species}</span>
+        </div>
+      ))}
+    </div>
+  );
 }
+}
+
--- 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
+++ 08-React-Estado-LifeCycle/homework/01 - Exercises/src/components/Zoo/Zoo.jsx
@@ -1,17 +1,81 @@
-import React from "react";
-// eslint-disable-next-line no-unused-vars
+import React, { useEffect, useState, useRef } from "react";
 import Animals from "../Animals/Animals";
-// eslint-disable-next-line no-unused-vars
 import Species from "../Species/Species";
-// import styledZoo from "./Zoo.module.css";
+import styledZoo from "./Zoo.module.css";
 
 export default function Zoo() {
-  /* Escribe acá tu código */
+  const [zoo, setZoo] = useState({
+    zooName: "",
+    animals: [],
+    species: [],
+  });
 
+  const allAnimalsRef = useRef([]);
+
+  useEffect(() => {
+    // Utilizando fetch para obtener datos del servidor
+    fetch("http://localhost:3001/zoo")
+      .then((res) => res.json())
+      .then((data) => {
+        setZoo((prevZoo) => ({
+          ...prevZoo,
+          animals: data.animals,
+          species: data.species,
+        }));
+        allAnimalsRef.current = data.animals;
+      })
+      .catch((error) => console.log(error));
+  }, []);
+
+  const handleSpecies = (selectedSpecie) => {
+    const filteredAnimals = allAnimalsRef.current.filter(
+      (animal) => animal.species === selectedSpecie
+    );
+
+    setZoo((prevZoo) => ({
+      ...prevZoo,
+      animals: filteredAnimals,
+    }));
+  };
+
+  const handleAllSpecies = () => {
+    // Verificar si ya estamos mostrando todos los animales
+    if (zoo.animals !== allAnimalsRef.current) {
+      // Mostrar todos los animales solo si aún no lo estamos haciendo
+      setZoo((prevZoo) => ({
+        ...prevZoo,
+        animals: allAnimalsRef.current,
+      }));
+    }
+  };
+
+  const handleInputChange = (event) => {
+    setZoo({
+      ...zoo,
+      zooName: event.target.value,
+    });
+  };
 
   return (
-    <div>
-      <h1>-</h1>
+    <div className={styledZoo.divContent}>
+      <div className={styledZoo.divContentTitle}>
+        <label>Zoo Name:</label>
+        <input
+          type="text"
+          value={zoo.zooName}
+          onChange={handleInputChange}
+        />
+        <h1 className={styledZoo.title}>{zoo.zooName}</h1>
+      </div>
+      {/* Renderizar el componente Species */}
+      <Species
+        species={zoo.species}
+        handleSpecies={handleSpecies}
+        handleAllSpecies={handleAllSpecies}
+      />
+
+      {/* Renderizar el componente Animals */}
+      <Animals animals={zoo.animals} />
     </div>
   );
 }
